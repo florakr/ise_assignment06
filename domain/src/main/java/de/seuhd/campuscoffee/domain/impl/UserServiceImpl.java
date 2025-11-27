7aeeaea18e7192ac249f2ac6e3852a960ac46321
flora.kruschke <flora.kruschke@gmail.com>
2025-11-27 17:26:05 +0100

    implementation of missing interfaces and service implementations

--- domain/src/main/java/de/seuhd/campuscoffee/domain/impl/UserServiceImpl.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/impl/UserServiceImpl.java
@@ -1,13 +1,87 @@
 package de.seuhd.campuscoffee.domain.impl;
 
+import de.seuhd.campuscoffee.domain.model.User;
 import de.seuhd.campuscoffee.domain.ports.UserService;
+import de.seuhd.campuscoffee.domain.ports.UserDataService;
+
+
+import de.seuhd.campuscoffee.domain.exceptions.DuplicationException;
+import de.seuhd.campuscoffee.domain.exceptions.MissingFieldException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+import java.util.Objects;
+
+ /**
+  * Implementation of the User service that handles business logic related to User entities.
+  */
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    // TODO: Implement user service
+
+    private final UserDataService userDataService;
+
+    @Override
+    public void clear() {
+        log.warn("Clearing all user data");
+        userDataService.clear();
+    }
+
+    @Override
+    public @NonNull List<User> getAll() {
+        log.debug("Retrieving all users");
+        return userDataService.getAll();
+    }
+
+    @Override
+    public @NonNull User getById(@NonNull Long id) {
+        log.debug("Retrieving user with ID: {}", id);
+        return userDataService.getById(id);
+    }
+
+    @Override
+    public @NonNull User getByLoginName(@NonNull String loginName) {
+        log.debug("Retrieving user with login name: {}", loginName);
+        return userDataService.getByLoginName(loginName);
+    }
+
+    @Override
+    public @NonNull User upsert(@NonNull User user) {
+        if (user.id() == null) {
+            // create a new user
+            log.info("Creating new user: {}", user.loginName());
+        } else {
+            // update existing user
+            log.info("Updating user with ID: {}", user.id());
+            // user ID must be set
+            Objects.requireNonNull(user.id());
+            // user must exist in the database before the update
+            userDataService.getById(user.id());
+        }
+        return performUpsert(user);
+    }
+
+    @Override
+    public void delete(@NonNull Long id) {
+        log.info("Trying to delete user witj ID: {}", id);
+        userDataService.delete(id);
+        log.info("Deleted user with ID: {}", id);
+    }
+
+    private @NonNull User performUpsert(User user) {
+        try {
+            User upsertedUser = userDataService.upsert(user);
+            log.info("Successfully upserted user with ID: {}", upsertedUser.id());
+            return upsertedUser;
+        } catch (DuplicationException e) {
+            log.error("Error upserting user '{}': {}", user.loginName(), e.getMessage());
+            throw e;
+        }
+    }
 }
--- domain/src/main/java/de/seuhd/campuscoffee/domain/ports/UserService.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/ports/UserService.java
@@ -1,5 +1,61 @@
 package de.seuhd.campuscoffee.domain.ports;
 
+import de.seuhd.campuscoffee.domain.model.User;
+import org.jspecify.annotations.NonNull;
+import java.util.List;
+
+
 public interface UserService {
-    //TODO: Define user service interface
+
+    /**
+     * Clears all user data from the system.
+     * This is typically used for testing or administrative purposes.
+     * Warning: This operation is destructive and cannot be undone.
+     */
+    void clear();
+
+    /**
+     * Retrieves all users from the system.
+     *
+     * @return a list of all users in the system; never null, but may be empty if no users exist
+     */
+    @NonNull List<User> getAll();
+
+    /**
+     * Retrieves a specific user by their unique identifier.
+     *
+     * @param id the unique identifier of the user to retrieve; must not be null
+     * @return the user with the specified ID; never null
+     */
+    @NonNull User getById(@NonNull Long id);
+
+    /**
+     * Retrieves a specific user by their unique login name.
+     *
+     * @param loginName the unique login name of the user to retrieve; must not be null
+     * @return the user with the specified login name; never null
+     */
+    @NonNull User getByLoginName(@NonNull String loginName);
+
+    /**
+     * Creates a new user or updates an existing one.
+     * This method performs an "upsert" operation:
+     * <ul>
+     *   <li>If the user has no ID (null), a new user is created</li>
+     *   <li>If the user has an ID, and it exists, the existing user is updated</li>
+     * </ul>
+     *
+     * @param user the user to create or update; must not be null
+     * @return the persisted user entity with updated timestamps and ID; never null
+     */
+    @NonNull User upsert(@NonNull User user);
+
+    /**
+     * Deletes a user by their unique identifier.
+     *
+     * @param id the unique identifier of the user to delete; must not be null
+     */
+
+    void delete(@NonNull Long id);
+
 }
