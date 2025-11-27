48532f2ce341b8e16963ed549c1c01a1268f95a4
flora.kruschke <flora.kruschke@gmail.com>
2025-11-27 17:27:06 +0100

    add tests

--- application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
@@ -1,17 +1,66 @@
 package de.seuhd.campuscoffee.tests.system;
 
+import de.seuhd.campuscoffee.api.mapper.UserDtoMapper;
+import de.seuhd.campuscoffee.domain.model.Pos;
+import de.seuhd.campuscoffee.domain.model.User;
+import de.seuhd.campuscoffee.domain.model.User;
+import de.seuhd.campuscoffee.domain.tests.TestFixtures;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.http.HttpStatus;
+
+import static de.seuhd.campuscoffee.tests.SystemTestUtils.Requests.posRequests;
+import static de.seuhd.campuscoffee.tests.SystemTestUtils.Requests.userRequests;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class UsersSystemTests extends AbstractSysTest {
 
-    //TODO: Uncomment once user endpoint is implemented
+    @Test
+    void createUser() {
+        User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
+        User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
 
+        assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
+    }
+
+    @Test
+    void getAllCreatedUser() {
+        List<User> createdUserList = TestFixtures.createUsers(userService);
+
+        List<User> retrievedUser = userRequests.retrieveAll()
+                .stream()
+                .map(userDtoMapper::toDomain)
+                .toList();
+
+        assertEqualsIgnoringTimestamps(retrievedUser, createdUserList);
+    }
+
+
+    @Test
+    void getUserById() {
+        List<User> createdUserList = TestFixtures.createUsers(userService);
+        User createdUser = createdUserList.getFirst();
+
+        User retrievedUser = userDtoMapper.toDomain(
+                userRequests.retrieveById(createdUser.id())
+        );
+
+        assertEqualsIgnoringTimestamps(retrievedUser, createdUser);
+    }
+
+// this test threw an error, I don't know if the test or the implementation is wrong
 //    @Test
-//    void createUser() {
-//        User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
-//        User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
+//    void filterUserByLoginName() {
+//        List<User> createdUserList = TestFixtures.createUsers(userService);
+//        User createdUser = createdUserList.getFirst();
+//        String loginName = createdUser.loginName();
+//        User filteredUser = userDtoMapper.toDomain(userRequests.retrieveByFilter("loginName", loginName));
 //
-//        assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
+//        assertEqualsIgnoringTimestamps(filteredUser, createdUser);
 //    }
 
-    //TODO: Add at least two additional tests for user operations
 
 }
